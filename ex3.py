0cd158fab2e945ba070e4c4e4d699494388b2575
gayetlyan <90770999+gayetlyan@users.noreply.github.com>
2023-11-14 15:22:36 +0100

    exo 3 part 2

--- ex3.py
+++ ex3.py
@@ -5,5 +5,12 @@ nombre2 = float(input("Entrez le deuxième nombre : "))
 def addition(a, b):
     return a + b
 
+def soustraction(a, b):
+    return a - b
 
 print(f"Addition : {addition(nombre1, nombre2)}")
+print(f"Soustraction : {soustraction(nombre1, nombre2)}")
+
+
+
+
