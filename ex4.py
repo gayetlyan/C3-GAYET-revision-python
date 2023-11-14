d4dae224d0ae7370db56aad59dc40425d754d402
gayetlyan <90770999+gayetlyan@users.noreply.github.com>
2023-11-14 15:44:22 +0100

    exo 4 part 1

---
+++ ex4.py
@@ -0,0 +1,10 @@
+liste_nombres = [23, 45, 12, 67, 89, 34, 56, 78, 90, 10]
+print("Liste de nombres :", liste_nombres)
+
+def trouver_maximum(liste):
+    return max(liste)
+
+max = trouver_maximum(liste_nombres)
+print("Maximum :", max)
+
+
