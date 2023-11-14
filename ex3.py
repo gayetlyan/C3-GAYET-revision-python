df3106e15a08bdfc886f30874b28ce6e82bf99aa
gayetlyan <90770999+gayetlyan@users.noreply.github.com>
2023-11-14 15:21:03 +0100

    exo3 part 1

---
+++ ex3.py
@@ -0,0 +1,9 @@
+nombre1 = float(input("Entrez le premier nombre : "))
+nombre2 = float(input("Entrez le deuxième nombre : "))
+
+
+def addition(a, b):
+    return a + b
+
+
+print(f"Addition : {addition(nombre1, nombre2)}")
