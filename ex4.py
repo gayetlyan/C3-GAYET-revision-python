65c0055a6f8221ce9d274b44235b644f13bc2f5a
gayetlyan <90770999+gayetlyan@users.noreply.github.com>
2023-11-14 16:18:19 +0100

    exo 4 sans min() et max()

--- ex4.py
+++ ex4.py
@@ -2,21 +2,31 @@ liste_nombres = [23, 45, 12, 67, 89, 34, 56, 78, 90, 10]
 print("Liste de nombres :", liste_nombres)
 
 def trouver_maximum(liste):
-    return max(liste)
+    maximum = liste[0]
+    for nombre in liste:
+        if nombre > maximum:
+            maximum = nombre
+    return maximum
 
 max = trouver_maximum(liste_nombres)
 print("Maximum :", max)
 
 
 def trouver_minimum(liste):
-    return min(liste)
+    minimum = liste[0]
+    for nombre in liste:
+        if nombre < minimum:
+            minimum = nombre
+    return minimum
 
 min = trouver_minimum(liste_nombres)
 print("Minimum :", min)
 
 
 def calculer_moyenne(liste):
-    return sum(liste) / len(liste)
+    somme = sum(liste)
+    moyenne = somme / len(liste)
+    return moyenne
 
 moyenne = calculer_moyenne(liste_nombres)
 print("Moyenne :", moyenne)
